eff35f56e7e53aa0ec1fa665580aa923202a9555
Daryl_Moh <Daryl.Moh@hotmail.com>
2023-05-23 00:04:50 +0800

    paypal works

---
+++ paypall/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgxPayPalModule } from 'ngx-paypal';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxPayPalModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
